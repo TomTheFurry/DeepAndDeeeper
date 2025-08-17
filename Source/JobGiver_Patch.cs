9ad1fc85c5355bd89ce7a8cc7e36274d231bb8f9
icosahedron1315 <225608120+icosahedron1315@users.noreply.github.com>
2025-08-17 17:02:41 +0200

    Fix exit job not being created if no food found
    - How to make a postfix with Harmony which adds a job if no ingest job is created. So essentially, I think the pawn will try to ingest something, can't find food, and then we exit the cave.

--- Source/JobGiver_Patch.cs
+++ Source/JobGiver_Patch.cs
@@ -11,23 +11,18 @@ namespace Shashlichnik
 
         private static void TryOverrideJob(Pawn pawn, ref Job job)
         {
-            Log.Message($"[JobGiver_Patch] Checking job for pawn {pawn.Name}: [{job?.def?.defName}]");
-            
+            // Log.Message($"job is [{job?.def?.defName}]");
             if (job?.def?.defName is { } val
                 && overridingJobDefNames.Contains(val))
             {
-                Log.Message($"[JobGiver_Patch] Found overridable job: {val}");
-                
                 var caveComp = pawn.Map.GetComponent<CaveMapComponent>();
-                Log.Message($"[JobGiver_Patch] Cave component: {caveComp != null}, exit: {caveComp?.caveExit != null}, spawned: {caveComp?.caveExit?.Spawned}, exitIfNoJob: {caveComp?.caveExit?.exitIfNoJob}");
-                
                 if (caveComp != null && caveComp.caveExit != null && caveComp.caveExit.Spawned &&
                     caveComp.caveExit.exitIfNoJob)
                 {
                     // Check if this is a hunger-related job and pawn can't find food in the cave
                     if (val == "Ingest" && !CanFindFoodInCave(pawn, caveComp))
                     {
-                        Log.Message($"[JobGiver_Patch] Detected hunger but no food available in cave. Redirecting pawn to exit cave.");
+                        Log.Message($"Detected hunger but no food available in cave. Redirecting pawn to exit cave.");
                         var oldjob = job;
                         JobMaker.ReturnToPool(oldjob); // Return the old job to the pool
                         // make a new one
@@ -36,20 +31,12 @@ namespace Shashlichnik
                     // Check if this is sleeping on floor (original functionality)
                     else if (val == "LayDown")
                     {
-                        Log.Message($"[JobGiver_Patch] Detected overridable job [{val}]. Redirecting the pawn to exit cave instead.");
+                        Log.Message($"Detected overridable job [{val}]. Redirecting the pawn to exit cave instead.");
                         var oldjob = job;
                         JobMaker.ReturnToPool(oldjob); // Return the old job to the pool
                         // make a new one
                         job = JobMaker.MakeJob(JobDefOf.EnterPortal, caveComp.caveExit);
                     }
-                    else
-                    {
-                        Log.Message($"[JobGiver_Patch] Job {val} not handled or conditions not met");
-                    }
-                }
-                else
-                {
-                    Log.Message($"[JobGiver_Patch] Cave exit conditions not met for override");
                 }
             }
         }
@@ -73,7 +60,6 @@ namespace Shashlichnik
         {
             public static void Postfix(Pawn pawn, ref Job __result)
             {
-                Log.Message($"[JobGiver_Patch] GetRest patch called for {pawn.Name}, result: {__result?.def?.defName}");
                 TryOverrideJob(pawn, ref __result);
             }
         }
@@ -84,7 +70,18 @@ namespace Shashlichnik
         {
             public static void Postfix(Pawn pawn, ref Job __result)
             {
-                Log.Message($"[JobGiver_Patch] GetFood patch called for {pawn.Name}, result: {__result?.def?.defName}");
+                // If no food job was created, check if we should exit cave due to hunger
+                if (__result == null)
+                {
+                    var caveComp = pawn.Map.GetComponent<CaveMapComponent>();
+                    if (caveComp != null && caveComp.caveExit != null && caveComp.caveExit.Spawned &&
+                        caveComp.caveExit.exitIfNoJob && !CanFindFoodInCave(pawn, caveComp))
+                    {
+                        Log.Message($"{pawn.Name} found no food in cave. Giving exit job.");
+                        __result = JobMaker.MakeJob(JobDefOf.EnterPortal, caveComp.caveExit);
+                    }
+                }
+                
                 TryOverrideJob(pawn, ref __result);
             }
         }
